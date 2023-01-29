d7a0257019744dc2a47017b5d0f7822e8b5154b1
Alexander <aleksbogac@gmail.com>
2023-01-29 23:27:10 +0200

    Initial commit

---
+++ src/main/java/yummypizza/YummyPizzaApplication.java
@@ -0,0 +1,13 @@
+package yummypizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummyPizzaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/yummypizza/config/SpringCoreConfiguration.java
@@ -0,0 +1,44 @@
+package yummypizza.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringCoreConfiguration {
+
+    @Value("${database.url}")
+    private String databaseUrl;
+
+    @Value("${database.username}")
+    private String username;
+
+    @Value("${database.password}")
+    private String password;
+
+    @Value("${driver.className}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(databaseUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+        return dataSource;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:/db/changelog/changelog-master.xml");
+        liquibase.setDataSource(dataSource());
+        return liquibase;
+    }
+
+}
