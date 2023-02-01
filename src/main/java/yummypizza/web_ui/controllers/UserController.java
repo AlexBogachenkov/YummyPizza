045c314c5829b234d758f5e2e01510287e466227
Alexander <aleksbogac@gmail.com>
2023-02-01 23:35:37 +0200

    Added User, AdminPage and HomePage controllers

---
+++ src/main/java/yummypizza/web_ui/controllers/UserController.java
@@ -0,0 +1,32 @@
+package yummypizza.web_ui.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import yummypizza.core.requests.SaveUserRequest;
+import yummypizza.core.responses.SaveUserResponse;
+import yummypizza.core.services.SaveUserService;
+
+@Controller
+@RequestMapping(value = "user")
+public class UserController {
+
+    @Autowired
+    private SaveUserService saveUserService;
+
+    @PostMapping
+    public String processSaveUserRequest(@ModelAttribute(value = "request")SaveUserRequest request, ModelMap modelMap) {
+        SaveUserResponse response = saveUserService.execute(request);
+        if (response.hasErrors()) {
+            modelMap.addAttribute("errors", response.getErrors());
+            return "adminPage.html";
+        } else {
+            modelMap.addAttribute("response", response.getSavedUser().getId());
+            return "adminPage.html";
+        }
+    }
+
+}
