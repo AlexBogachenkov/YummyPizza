ff9fd9ba9267e512e536e995307011a5a87b7b79
Alexander <aleksbogac@gmail.com>
2023-02-03 00:37:15 +0200

    Changed structure of adminPage.html actions

--- src/main/java/yummypizza/web_ui/controllers/UserController.java
+++ src/main/java/yummypizza/web_ui/controllers/UserController.java
@@ -3,29 +3,38 @@ package yummypizza.web_ui.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import yummypizza.core.requests.SaveUserRequest;
 import yummypizza.core.responses.SaveUserResponse;
 import yummypizza.core.services.SaveUserService;
 
 @Controller
-@RequestMapping(value = "users")
 public class UserController {
 
     @Autowired
     private SaveUserService saveUserService;
 
-    @PostMapping
+    @GetMapping(value = "users")
+    public String showUsersPage() {
+        return "users/users.html";
+    }
+
+    @GetMapping(value = "usersCreate")
+    public String showUsersCreatePage(ModelMap modelMap) {
+        modelMap.addAttribute("request", new SaveUserRequest());
+        return "users/usersCreate.html";
+    }
+
+    @PostMapping(value = "usersCreate")
     public String processSaveUserRequest(@ModelAttribute(value = "request")SaveUserRequest request, ModelMap modelMap) {
         SaveUserResponse response = saveUserService.execute(request);
         if (response.hasErrors()) {
             modelMap.addAttribute("errors", response.getErrors());
-            return "adminPage.html";
+            return "users/usersCreate.html";
         } else {
-            modelMap.addAttribute("response", response.getSavedUser().getId());
-            return "adminPage.html";
+            return "users/users.html";
         }
     }
 
