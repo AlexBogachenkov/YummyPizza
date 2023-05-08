e85584b32c89e8b37676ede984c611b8a5fca2fb
Alexander <aleksbogac@gmail.com>
2023-05-08 22:51:59 +0300

    Added CartProduct domain class

---
+++ src/main/java/yummypizza/core/domain/CartProduct.java
@@ -0,0 +1,31 @@
+package yummypizza.core.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cart_products")
+public class CartProduct {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", referencedColumnName = "id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id", nullable = false)
+    private Product product;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+
+}
