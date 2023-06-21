e07e75bd1ce340abfe6f4c8774964377746e4a4c
Alexander <aleksbogac@gmail.com>
2023-06-21 20:02:04 +0300

    Added cart automatic creation for new users

--- src/main/java/yummypizza/core/services/user/CreateUserService.java
+++ src/main/java/yummypizza/core/services/user/CreateUserService.java
@@ -3,7 +3,10 @@ package yummypizza.core.services.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import yummypizza.core.database.CartRepository;
 import yummypizza.core.database.UserRepository;
+import yummypizza.core.domain.Cart;
+import yummypizza.core.domain.CartStatus;
 import yummypizza.core.domain.User;
 import yummypizza.core.requests.user.CreateUserRequest;
 import yummypizza.core.responses.CoreError;
@@ -18,7 +21,9 @@ public class CreateUserService {
     @Autowired
     private CreateUserRequestValidator validator;
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
+    @Autowired
+    private CartRepository cartRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
 
@@ -27,9 +32,13 @@ public class CreateUserService {
         if (!errors.isEmpty()) {
             return new CreateUserResponse(errors);
         }
+
         User user = new User(request.getFirstName(), request.getLastName(), request.getEmail(),
                 passwordEncoder.encode(request.getPassword()), request.getPhone(), request.getRole());
-        return new CreateUserResponse(repository.save(user));
+        User createdUser = userRepository.save(user);
+
+        cartRepository.save(new Cart(createdUser, CartStatus.ACTIVE));
+        return new CreateUserResponse(createdUser);
     }
 
 }
