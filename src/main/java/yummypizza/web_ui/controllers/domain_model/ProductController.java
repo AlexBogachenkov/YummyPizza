1e81bb1b4e07ab08dbe19edbb328ca62eaa1966c
Alexander <aleksbogac@gmail.com>
2023-06-23 16:21:58 +0300

    Revert "Added button to add product to the cart from the product page"
    This reverts commit 110074e94479d486fff484ff5316248ef656a4c8.

--- src/main/java/yummypizza/web_ui/controllers/domain_model/ProductController.java
+++ src/main/java/yummypizza/web_ui/controllers/domain_model/ProductController.java
@@ -1,32 +1,18 @@
 package yummypizza.web_ui.controllers.domain_model;
 
-import jakarta.annotation.security.PermitAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import yummypizza.core.domain.CartProduct;
-import yummypizza.core.domain.CartStatus;
 import yummypizza.core.domain.Product;
-import yummypizza.core.requests.cart.FindCartsByUserIdAndStatusRequest;
-import yummypizza.core.requests.cart_product.AddCartProductRequest;
-import yummypizza.core.requests.cart_product.FindCartProductsByCartIdRequest;
 import yummypizza.core.requests.product.CreateProductRequest;
 import yummypizza.core.requests.product.DeleteProductByIdRequest;
 import yummypizza.core.requests.product.FindProductByIdRequest;
 import yummypizza.core.requests.product.UpdateProductRequest;
-import yummypizza.core.responses.cart.FindCartsByUserIdAndStatusResponse;
-import yummypizza.core.responses.cart_product.FindCartProductsByCartIdResponse;
 import yummypizza.core.responses.product.*;
-import yummypizza.core.services.cart.FindCartsByUserIdAndStatusService;
-import yummypizza.core.services.cart_product.FindCartProductsByCartIdService;
 import yummypizza.core.services.product.*;
-import yummypizza.security.CustomUserDetails;
 
-import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -43,11 +29,6 @@ public class ProductController {
     private FindProductByIdService findProductByIdService;
     @Autowired
     private UpdateProductService updateProductService;
-    @Autowired
-    private FindCartsByUserIdAndStatusService findCartsByUserIdAndStatusService;
-    @Autowired
-    private FindCartProductsByCartIdService findCartProductsByCartIdService;
-
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping(value = "")
@@ -102,24 +83,8 @@ public class ProductController {
         return "products/productsFindById.html";
     }
 
-    @PermitAll
     @GetMapping(value = "/{id}")
     public String processFindProductByIdRequest(@PathVariable(value = "id", required = false) Long id, ModelMap modelMap) {
-        Optional<CustomUserDetails> optionalOfUserDetails = getAuthenticatedUserDetails();
-        if (optionalOfUserDetails.isPresent()) {
-            CustomUserDetails userDetails = optionalOfUserDetails.get();
-            FindCartsByUserIdAndStatusRequest findCartsRequest = new FindCartsByUserIdAndStatusRequest(userDetails.getId(), CartStatus.ACTIVE);
-            FindCartsByUserIdAndStatusResponse findCartsResponse = findCartsByUserIdAndStatusService.execute(findCartsRequest);
-            modelMap.addAttribute("cart", findCartsResponse.getCarts().get(0));
-
-            FindCartProductsByCartIdRequest findCartProductsRequest =
-                    new FindCartProductsByCartIdRequest(findCartsResponse.getCarts().get(0).getId());
-            FindCartProductsByCartIdResponse findCartProductsResponse = findCartProductsByCartIdService.execute(findCartProductsRequest);
-            List<CartProduct> userCartProducts = findCartProductsResponse.getCartProducts();
-            modelMap.addAttribute("userCartProducts", userCartProducts);
-        } else modelMap.addAttribute("cart", null);
-        modelMap.addAttribute("addProductToCartRequest", new AddCartProductRequest());
-
         FindProductByIdRequest request = new FindProductByIdRequest(id);
         FindProductByIdResponse response = findProductByIdService.execute(request);
         if (response.hasErrors()) {
@@ -160,14 +125,6 @@ public class ProductController {
             modelMap.addAttribute("updatedProduct", response.getUpdatedProduct());
         }
         return "products/productsUpdate.html";
-    }
-
-    private Optional<CustomUserDetails> getAuthenticatedUserDetails() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication.isAuthenticated() && authentication.getPrincipal() instanceof CustomUserDetails) {
-            return Optional.of((CustomUserDetails) authentication.getPrincipal());
-        }
-        return Optional.empty();
     }
 
 }
